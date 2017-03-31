552ee7ec401f960b4c9bb311ee4c328a2b57a1d1
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-31 09:05:22 +0100

    Adds file to search healines in Api using a keyword

---
+++ public/javascripts/SearchHeadlines.js
@@ -0,0 +1,18 @@
+function SearchHeadlines() {
+
+}
+
+SearchHeadlines.prototype.getHeadlines = function(dataDisplayNews) {
+  var http = new XMLHttpRequest();
+  var url = "http://localhost:3000/search";
+  http.open("POST", url, true);
+  http.setRequestHeader("Content-type", "application/json");
+  http.onreadystatechange = function() {
+      if(http.readyState == 4 && http.status == 200) {
+        console.log(http.responseText)
+        this.text = JSON.parse(http.responseText)
+        console.log(this.text.results[0].title.title)
+      }
+  }
+  http.send();
+}
