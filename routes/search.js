7779c2146a6245f0b93c728646f289556422c05f
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-31 13:08:15 +0100

    Adds response to no articles found

--- routes/search.js
+++ routes/search.js
@@ -4,49 +4,42 @@ var request = require("request");
 
 
 router.route('/')
-  .post(function(req, res, next) {
-    // console.log(req.body.q)
+.post(function(req, res, next) {
   mySearch = {
-   search:req.body.q,
- };
+    search:req.body.q,
+  };
 
- request({
-   uri: 'http://api.ft.com/content/search/v1?apiKey=ercgrchspx5ku37xnzngy5qq',
-   json: true,
-   method: 'POST',
-   body: {
-     "queryString": mySearch.search,
-     "queryContext" : {
-    "curations" : [ "ARTICLES"]},
-     "resultContext" : {
-    "aspects" :[  "title","lifecycle","location","summary","editorial" ]}
-     }
+  request({
+    uri: 'http://api.ft.com/content/search/v1?apiKey=ercgrchspx5ku37xnzngy5qq',
+    json: true,
+    method: 'POST',
+    body: {
+      "queryString": mySearch.search,
+      "queryContext" : {
+        "curations" : [ "ARTICLES"]},
+        "resultContext" : {
+          "aspects" :[  "title","lifecycle","location","summary","editorial" ]}
+        }
 
-   }, function (error, response, body) {
+      }, function (error, response, body) {
+        res.format({
+          html: function(){
+            if(body.results[0].indexCount > 0){
+              var myArray = []
+              for(var i = 0; i < body.results[0].results.length; i ++ ) {
+                myArray.push(body.results[0].results[i].title)
+              }
+            } else {
+              myArray = [{title: "There are no articles on " + mySearch.search + ". Please search again"}]
+            }
+            res.render('results', {
+              "repo": myArray
+            });
+          },
+        });
 
-     res.format({
-                    html: function(){
-                      var myArray = []
-      for(var i = 0; i < body.results[0].results.length; i ++ ) {
-         myArray.push(body.results[0].results[i].title)
-      }
-      console.log(myArray)
-                        res.render('results', {
 
-"repo": myArray
+      });
+    })
 
-
-
-
-                          });
-                    },
-                });
-
-    //  console.log(mySearch.search)
-    //   var jsonStr = JSON.stringify(body.results[0]);
-    //   res.(jsonStr)
-    // console.log('Follow: ' + team.join(' ') + '!');
-   });
-})
-
-module.exports = router;
+    module.exports = router;
